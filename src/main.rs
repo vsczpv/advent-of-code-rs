67915ff58001d8aaacb2ec8ba4dfa8693aaa88bf
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-11-30 23:29:20 -0300

    ffix: Use better syntax on match
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let day = &args[1];
     match day.as_str() {
-        "day15p1" => { day15::main(Part::One); }
-        "day15p2" => { day15::main(Part::Two); }
-        _         => { println!("Non-existent day."); }
+        "day15p1" => day15::main(Part::One),
+        "day15p2" => day15::main(Part::Two),
+        _         => println!("Non-existent day.")
     }
 }
