3a9839e4852b651ce645737fb3526c48765794c1
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-12-06 17:34:37 -0300

    aoc2024: Solve Day 6, part two.
    Brute force go brrrrrr.
    
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- src/aoc2024/day06.rs
+++ src/aoc2024/day06.rs
@@ -10,6 +10,7 @@ pub fn main(_part: Part) {
 	let mut grid: Vec<Vec<char>> = Vec::new();
 
 	let (mut where_x, mut where_y) = (0, 0);
+
 	for (y, line) in file.lines().enumerate() {
 		grid.push(Vec::new());
 		for (x, c) in line.chars().enumerate() {
@@ -20,6 +21,9 @@ pub fn main(_part: Part) {
 			}
 		}
 	}
+
+	let starting_grid      = grid.clone();
+	let (start_x, start_y) = (where_x, where_y);
 
 	static DELTA_M: phf::Map<char, (isize, isize)> = phf_map! {
 		'^' => ( 0, -1),
@@ -56,7 +60,87 @@ pub fn main(_part: Part) {
 			.map(|x| { x.iter().map(|y| { (*y == 'X') as i32 }).sum::<i32>() })
 			.sum();
 
-	println!("final result is {count}");
+	println!("(part1) final result is {count}");
+
+	let mut candidates: Vec<(usize, usize)> = Vec::new();
+
+	for (y, row) in grid.iter().enumerate() {
+		for (x, col) in row.iter().enumerate() {
+			if *col == 'X' {
+				candidates.push((x, y));
+			}
+		}
+	}
+
+	let mut moves: Vec<Move> = Vec::new();
+	let mut count = 0;
+
+	for can in candidates {
+		let mut new_grid = starting_grid.clone();
+		if can.1 == start_y && can.0 == start_x { continue; }
+		new_grid[can.1][can.0] = '#';
+		where_x = start_x;
+		where_y = start_y;
+		let cycles = loop {
+			let guard = new_grid[where_y][where_x];
+
+			moves.push(Move::new(guard, where_x, where_y));
+
+			if cyclecheck(&moves) {
+				break true;
+			}
+
+			let dm = DELTA_M.get(&guard).unwrap();
+
+			let next = (where_x as isize + dm.0, where_y as isize + dm.1);
+			let (next_x, next_y) = (next.0 as usize, next.1 as usize);
+
+			if !boundscheck(next_x, next_y, &new_grid) { break false; }
+			let next = new_grid[next_y][next_x];
+
+			if next == '#' {
+				new_grid[where_y][where_x] = turn(guard);
+			} else {
+				where_x = next_x;
+				where_y = next_y;
+				new_grid[where_y][where_x] = guard;
+			}
+		};
+		if cycles {
+			println!("loops at {} {}", can.0, can.1);
+			count += 1;
+		}
+		moves.clear();
+	}
+
+	println!("(part2) final result is {count}");
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct Move {
+	guard: char,
+	x:     usize,
+	y:     usize
+}
+
+impl Move {
+	fn new(guard: char, x: usize, y: usize) -> Self { Self { guard, x, y } }
+}
+
+fn cyclecheck(data: &Vec<Move>) -> bool {
+
+	let l = data.len();
+	for w in 1..(l/2+1) {
+		let a = &data.as_slice()[l-w..l];
+		let b = &data.as_slice()[l-w*2..l-w];
+		let mut equal = true;
+		for i in 0..w {
+			if a[i] != b[i] { equal = false; break; }
+		}
+		if equal { return true; }
+	}
+
+	return false;
 }
 
 fn printgrid(grid: &Vec<Vec<char>>) {
