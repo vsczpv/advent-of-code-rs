bc629c8f72ad569a2d0bd0d19dad744a36a7f861
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-12-09 19:55:03 -0300

    aoc2024: Solve Day 9, part two.
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- src/aoc2024/day09.rs
+++ src/aoc2024/day09.rs
@@ -2,7 +2,7 @@
 
 use crate::common::Part;
 
-pub fn main(_part: Part) {
+pub fn main(part: Part) {
 
 	let file = std::fs::read_to_string("inputs/i24day09p1.txt").unwrap();
 
@@ -23,46 +23,86 @@ pub fn main(_part: Part) {
 	let mut j = disk.len()-1;
 
 	let forward  = |x: &mut usize, disk: &Vec<Option<u32>>| while disk[*x] != None { *x += 1; };
-	let backward = |x: &mut usize, disk: &Vec<Option<u32>>| while disk[*x] == None { *x -= 1; };
+	let backward = |x: &mut usize, disk: &Vec<Option<u32>>| while disk[*x] == None { if *x == 0 { return; } else { *x -= 1; } };
 
-	forward (&mut i, &disk);
-	backward(&mut j, &disk);
+	fn backward_until(x: &mut usize, disk: &Vec<Option<u32>>) {
+		let dig = disk[*x];
+		if dig == None {
+			*x -= 1;
+			backward_until(x, disk);
+			return;
+		}
+		while disk[*x] == dig {
+			if *x == 0 { return; } else { *x -= 1; }
+		}
+		*x += 1;
+	}
 
-	loop {
+	match part {
+		Part::One => {
+			forward (&mut i, &disk);
+			backward(&mut j, &disk);
 
-		if j <= i { break; }
+			loop {
 
-		let aux = disk[i];
-		disk[i] = disk[j];
-		disk[j] = aux;
+				if j <= i { break; }
 
-		forward (&mut i, &disk);
-		backward(&mut j, &disk);
+				let aux = disk[i];
+				disk[i] = disk[j];
+				disk[j] = aux;
 
-		/*
-		for d in &disk {
-			if *d == None {
-				print!(".");
-			} else {
-				print!("{}", d.unwrap());
+				forward (&mut i, &disk);
+				backward(&mut j, &disk);
+
+			}
+		},
+		Part::Two => {
+
+			let count  = |mut x: usize, disk: &Vec<Option<u32>>| {
+				let mut count = 0;
+				let dig       = disk[x];
+				while disk[x] == dig {
+					x += 1;
+					count += 1;
+					if x >= disk.len() { break; }
+				} count
+			};
+
+			forward (&mut i, &disk);
+			let mut a_len = count(i, &disk);
+			backward_until(&mut j, &disk);
+			let mut b_len = count(j, &disk);
+
+			loop {
+
+				if j > i {
+					if a_len < b_len {
+						i += 1;
+						forward(&mut i, &disk);
+						a_len = count(i, &disk);
+						continue;
+					}
+
+					for k in 0..b_len {
+						disk[i+k] = disk[j+k];
+						disk[j+k] = None;
+					}
+				}
+
+				i  = 0;
+				forward (&mut i, &disk);
+				a_len = count(i, &disk);
+				j -= 1;
+				backward_until(&mut j, &disk);
+				if j == 0 { break; }
+				b_len = count(j, &disk);
 			}
 		}
-		println!("");
-		*/
-
 	}
-
-	/*
-	for d in &disk {
-		if *d == None { break; }
-		print!("{}", d.unwrap());
-	}
-	println!("");
-	*/
 
 	let mut sum: u64 = 0;
 	for (i, d) in disk.iter().enumerate() {
-		if *d == None { break; }
+		if *d == None { continue; }
 		sum += i as u64 * (d.unwrap() as u64);
 	}
 	println!("final result is {sum}");
